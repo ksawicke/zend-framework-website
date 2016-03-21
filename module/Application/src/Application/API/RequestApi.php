535312e98fa4197961dc77a82fc66bca67955d1d
Kevin Sawicke <sawik@NB011890.swift.com>
2016-03-21 07:30:05 -0700

    Clean up request API calls

--- module/Application/src/Application/API/RequestApi.php
+++ module/Application/src/Application/API/RequestApi.php
@@ -104,7 +104,6 @@ class RequestApi extends ApiController {
     public function submitTimeoffRequestAction()
     {
         $Employee = new \Request\Model\Employee();
-        $TimeoffRequests = new \Request\Model\TimeoffRequests();
         
         /** Clean up / append data to the Request **/
         $post = $this->getRequest()->getPost();
@@ -130,24 +129,9 @@ class RequestApi extends ApiController {
         
         /** Send email to employee and manager **/
         /** Grab data to email out **/
-        $timeoffRequestData = $TimeoffRequests->findRequest( $requestId );
-        $totalHoursRequested = $TimeoffRequests->countTimeoffRequested( $requestId );        
-        $hoursRequestedHtml = $TimeoffRequests->drawHoursRequested( $timeoffRequestData['ENTRIES'] );
-        $Email = new \Application\Factory\EmailFactory(
-            'Time off requested for ' . $post->request['forEmployee']['EMPLOYEE_DESCRIPTION_ALT'],
-            'A total of ' . $totalHoursRequested . ' hours were requested off for ' .
-                $post->request['forEmployee']['EMPLOYEE_DESCRIPTION_ALT'] . '<br /><br />' . 
-                $hoursRequestedHtml . '<br /><br />' .
-                'If you are a Supervisor, please review this request at the following URL:<br /><br />' .
-                '<a href="http://swift:10080/sawik/timeoff/public/request/review-request/' . $requestId .
-                '">http://swift:10080/sawik/timeoff/public/request/review-request/' . $requestId . '</a>',
-            ( ( ENVIRONMENT==='development' ) ? $this->developmentEmailAddressList : $post->request['forEmployee']['MANAGER_EMAIL_ADDRESS'] ),
-            ( ( ENVIRONMENT==='development' ) ? $this->developmentEmailAddressList : $post->request['forEmployee']['EMAIL_ADDRESS'] )
-            //'kevin_sawicke@swifttrans.com', // $post->request['forEmployee']['MANAGER_EMAIL_ADDRESS'],
-            //'kevin_sawicke@swifttrans.com' // $post->request['forEmployee']['EMAIL_ADDRESS']
-        );
-        $Email->send();
-        
+        $this->emailRequestToEmployee( $requestId );
+        $this->emailRequestToManager( $requestId );
+                
         if( $requestReturnData['request_id']!=null ) {
             $result = new JsonModel([
                 'success' => true,
@@ -163,6 +147,71 @@ class RequestApi extends ApiController {
         return $result;
     }
     
+    /**
+     * Get the total hours requested and block of HTML to insert into emails regarding the request.
+     * 
+     * @param integer $requestId
+     * @return array
+     */
+    protected function getEmailRequestVariables( $requestId )
+    {
+        $TimeoffRequests = new \Request\Model\TimeoffRequests();
+        $timeoffRequestData = $TimeoffRequests->findRequest( $requestId );
+        
+        return [ 'totalHoursRequested' => $TimeoffRequests->countTimeoffRequested( $requestId ),
+                 'hoursRequestedHtml' => $TimeoffRequests->drawHoursRequested( $timeoffRequestData['ENTRIES'] )
+               ];
+    }
+    
+    /**
+     * Send the employee an email confirming their request.
+     * 
+     * @param integer $requestId
+     */
+    protected function emailRequestToEmployee( $requestId )
+    {
+        $emailVariables = $this->getEmailRequestVariables( $requestId );
+        $Email = new \Application\Factory\EmailFactory(
+            'Time off requested for ' . $post->request['forEmployee']['EMPLOYEE_DESCRIPTION_ALT'],
+            'A total of ' . $emailVariables['totalHoursRequested'] . ' hours were requested off for ' .
+                $post->request['forEmployee']['EMPLOYEE_DESCRIPTION_ALT'] . '<br /><br />' . 
+                $emailVariables['hoursRequestedHtml'] . '<br /><br />' .
+                'If you are a Supervisor, please review this request at the following URL:<br /><br />' .
+                '<a href="http://swift:10080/sawik/timeoff/public/request/review-request/' . $requestId .
+                '">http://swift:10080/sawik/timeoff/public/request/review-request/' . $requestId . '</a>',
+            ( ( ENVIRONMENT==='development' ) ? $this->developmentEmailAddressList : $post->request['forEmployee']['MANAGER_EMAIL_ADDRESS'] ),
+            ( ( ENVIRONMENT==='development' ) ? $this->developmentEmailAddressList : $post->request['forEmployee']['EMAIL_ADDRESS'] )
+        );
+        $Email->send();
+    }
+    
+    /**
+     * Send 
+     * 
+     * @param integer $requestId
+     */
+    protected function emailRequestToManager( $requestId )
+    {
+        $emailVariables = $this->getEmailRequestVariables( $requestId );
+        $Email = new \Application\Factory\EmailFactory(
+            'Time off requested for ' . $post->request['forEmployee']['EMPLOYEE_DESCRIPTION_ALT'],
+            'A total of ' . $emailVariables['totalHoursRequested'] . ' hours were requested off for ' .
+                $post->request['forEmployee']['EMPLOYEE_DESCRIPTION_ALT'] . '<br /><br />' . 
+                $emailVariables['hoursRequestedHtml'] . '<br /><br />' .
+                'Please review this request at the following URL:<br /><br />' .
+                '<a href="http://swift:10080/sawik/timeoff/public/request/review-request/' . $requestId .
+                '">http://swift:10080/sawik/timeoff/public/request/review-request/' . $requestId . '</a>',
+            ( ( ENVIRONMENT==='development' ) ? $this->developmentEmailAddressList : $post->request['forEmployee']['MANAGER_EMAIL_ADDRESS'] ),
+            ( ( ENVIRONMENT==='development' ) ? $this->developmentEmailAddressList : $post->request['forEmployee']['EMAIL_ADDRESS'] )
+        );
+        $Email->send();
+    }
+    
+    /**
+     * Handles the manager approval process.
+     * 
+     * @return JsonModel
+     */
     public function submitApprovalResponseAction()
     {
         $request = $this->getRequest();
@@ -174,11 +223,7 @@ class RequestApi extends ApiController {
 
         $validationHelper = new \Request\Helper\ValidationHelper();
         $payrollReviewRequired = $validationHelper->isPayrollReviewRequired($requestData, $employeeData);
-        
-//        die( 'Time off request approved by ' . \Login\Helper\UserSession::getFullUserInfo() .
-//             ' for ' . trim(ucwords(strtolower($employeeData['EMPLOYEE_NAME'])) . '(' . trim($employeeData['EMPLOYEE_NUMBER']) . ')') .
-//             ( (!empty($request->getPost()->review_request_reason)) ? ' with the comment: ' . $request->getPost()->review_request_reason : '') );
-        
+                
         if($payrollReviewRequired===true) {
             $TimeoffRequestLog->logEntry(
                 $request->getPost()->request_id,
@@ -195,9 +240,6 @@ class RequestApi extends ApiController {
             $OutlookHelper = new \Request\Helper\OutlookHelper();
             $RequestEntry = new \Request\Model\RequestEntry();
             $Papaa = new \Request\Model\Papaa();
-
-//            die( 'Time off request approved by ' . \Login\Helper\UserSession::getFullUserInfo() .
-//                ' for ' . trim(ucwords(strtolower($employeeData['EMPLOYEE_NAME'])) . ' (' . $employeeData['EMPLOYEE_NUMBER'] . ')') );
             
             $calendarInviteData = $TimeoffRequests->findRequestCalendarInviteData($request->getPost()->request_id);
             
@@ -244,6 +286,11 @@ class RequestApi extends ApiController {
         return $result;
     }
     
+    /**
+     * Handles the manager deny process.
+     * 
+     * @return JsonModel
+     */
     public function submitDenyResponseAction()
     {
         $request = $this->getRequest();
