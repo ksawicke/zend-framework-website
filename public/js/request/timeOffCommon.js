2a9d2c3f22deb2ef95c67e93b09f70443296c669
Kevin Sawicke <kevin_sawicke@swifttrans.com>
2016-04-12 10:20:17 -0700

    Adjusted the validation of employee schedule edits Start form by pre validating.

--- public/js/request/timeOffCommon.js
+++ public/js/request/timeOffCommon.js
@@ -4,37 +4,30 @@
  */
 var timeOffCommon = new function ()
 {
-    var timeOffSubmitEmployeeScheduleRequestUrl = phpVars.basePath + '/api/employee-schedule';
-//    var dialogEditEmployeeSchedule = $( "#dialogEditEmployeeSchedule" ).dialog({
-//            autoOpen: false,
-//            height: 300,
-//            width: 350,
-//            modal: true,
-//            buttons: {
-//                "Create an account": timeOffCommon.submitEditEmployeeSchedule(),
-//                Cancel: function() {
-//                    dialog.dialog( "close" );
-//                }
-//            },
-//            close: function() {
-//                form[ 0 ].reset();
-//    //                allFields.removeClass( "ui-state-error" );
-//            }
-//        }),
-//        form = dialogEditEmployeeSchedule.find( "form" ).on( "submit", function( event ) {
-//            event.preventDefault();
-//            addUser();
-//        });
+    var timeOffSubmitEmployeeScheduleRequestUrl = phpVars.basePath + '/api/employee-schedule',
+        employeeScheduleFormErrors = 0;
     
+    /**
+     * What to run on initialize of this class.
+     * 
+     * @returns {undefined}
+     */
     this.initialize = function () {
         $(document).ready(function () {
             timeOffCommon.fadeOutFlashMessage();
             timeOffCommon.autoOpenDropdownOnHover();
             
-//            $("#navbar > ul > li > ul > li > a").on( 'click', function() {
-//                $('.dropdown ul').hide();
-//                $('li.dropdown.open').removeClass('open');
-//            });
+            $( "#dialogEditEmployeeSchedule" ).on( "dialogopen", function( event, ui ) {
+                $('#employeeScheduleForm').parsley().validate();
+            });
+            
+            $( "form#employeeScheduleForm :input" ).on( 'blur', function() {
+                if( timeOffCommon.checkFormValidates() ) {
+                    timeOffCommon.setEmployeeScheduleFormError( 'success' );
+                } else {
+                    timeOffCommon.setEmployeeScheduleFormError( 'error' );
+                }
+            });
             
             $( ".launchDialogEditEmployeeSchedule" ).on( 'click', function() {
                 $("#dialogEditEmployeeSchedule").dialog({
@@ -42,8 +35,21 @@ var timeOffCommon = new function ()
                     closeOnEscape: false,
                     buttons : {
                         Save : function() {
-                            $(this).dialog("close");
-                            timeOffCommon.submitEmployeeScheduleUpdate();
+                            if( $('#employeeScheduleForm').parsley().validate() &&
+                                $('#employeeScheduleForm').parsley().isValid() ) {
+                                timeOffCommon.setEmployeeScheduleFormError( 'success' );
+                                $(this).dialog("close");
+                                timeOffCommon.submitEmployeeScheduleUpdate();
+                            } else {
+                                timeOffCommon.setEmployeeScheduleFormError( 'error' );
+                            }        
+                            
+                            if( $('#employeeScheduleForm').parsley().validate() &&
+                                $('#employeeScheduleForm').parsley().isValid() ) {
+                                console.log( "Form looks good" );
+                            } else {
+                                console.log( "Form is sad." );
+                            }
                         },
                         Cancel : function() {
                             $(this).dialog("close");
@@ -51,40 +57,111 @@ var timeOffCommon = new function ()
                     }
                 });
             });
+            
+            $('#employeeScheduleForm').parsley( { showErrors: true } );
         });
     }
     
+    /**
+     * Sets error message for the Employee Schedule form.
+     * 
+     * @param {type} type
+     * @returns {undefined}
+     */
+    this.setEmployeeScheduleFormError = function (type) {
+        switch( type ) {
+            case 'error':
+                $('.employeeScheduleFormValidates').addClass('hidden');
+                $('.employeeScheduleDailyHoursExceeded').removeClass('hidden');
+                break;
+                
+            case 'uploadError':
+                $('.employeeScheduleFormValidates').addClass('hidden');
+                $('.employeeScheduleUploadError').removeClass('hidden');
+                break;
+                
+            case 'saveError':
+                $('.employeeScheduleFormValidates').addClass('hidden');
+                $('.employeeScheduleSaveError').removeClass('hidden');
+                break;
+                
+            case 'success':
+            default:
+                $('.employeeScheduleFormValidates').removeClass('hidden');
+                $('.employeeScheduleDailyHoursExceeded').addClass('hidden');
+                break;
+        }
+    }
+    
+    /**
+     * Validate the number of hours input.
+     * 
+     * @param {type} hours
+     * @returns {Boolean}
+     */
+    this.validateNumberHours = function( hours ) {
+        if( hours <= 12 && hours >= 0 ) {
+            return true;
+        }
+        return false;
+    }
+    
+    /**
+     * Check if the form validates
+     * 
+     * @returns {_L5.checkFormValidates.validates|Boolean}
+     */
+    this.checkFormValidates = function() {
+        var validates = false;
+        if( timeOffCommon.validateNumberHours( $("#employeeScheduleSUN").val() ) &&
+            timeOffCommon.validateNumberHours( $("#employeeScheduleMON").val() ) &&
+            timeOffCommon.validateNumberHours( $("#employeeScheduleTUE").val() ) &&
+            timeOffCommon.validateNumberHours( $("#employeeScheduleWED").val() ) &&
+            timeOffCommon.validateNumberHours( $("#employeeScheduleTHU").val() ) &&
+            timeOffCommon.validateNumberHours( $("#employeeScheduleFRI").val() ) &&
+            timeOffCommon.validateNumberHours( $("#employeeScheduleSAT").val() ) ) {
+                validates = true;
+        }
+        
+        return validates;
+    }
+    
     this.submitEmployeeScheduleUpdate = function () {
-        $.ajax({
-            url : timeOffSubmitEmployeeScheduleRequestUrl,
-            type : 'POST',
-            data : {
-                request : {
-                    forEmployee : {
-                        EMPLOYEE_NUMBER : $("#employeeScheduleFor").val(),
-                        SCHEDULE_SUN: $("#employeeScheduleSUN").val(),
-                        SCHEDULE_MON: $("#employeeScheduleMON").val(),
-                        SCHEDULE_TUE: $("#employeeScheduleTUE").val(),
-                        SCHEDULE_WED: $("#employeeScheduleWED").val(),
-                        SCHEDULE_THU: $("#employeeScheduleTHU").val(),
-                        SCHEDULE_FRI: $("#employeeScheduleFRI").val(),
-                        SCHEDULE_SAT: $("#employeeScheduleSAT").val()
-                    },
-                    byEmployee : $("#employeeScheduleBy").val()
+        var validates = timeOffCommon.checkFormValidates();
+        if( validates==true ) {
+            $.ajax({
+                url : timeOffSubmitEmployeeScheduleRequestUrl,
+                type : 'POST',
+                data : {
+                    request : {
+                        forEmployee : {
+                            EMPLOYEE_NUMBER : $("#employeeScheduleFor").val(),
+                            SCHEDULE_SUN: $("#employeeScheduleSUN").val(),
+                            SCHEDULE_MON: $("#employeeScheduleMON").val(),
+                            SCHEDULE_TUE: $("#employeeScheduleTUE").val(),
+                            SCHEDULE_WED: $("#employeeScheduleWED").val(),
+                            SCHEDULE_THU: $("#employeeScheduleTHU").val(),
+                            SCHEDULE_FRI: $("#employeeScheduleFRI").val(),
+                            SCHEDULE_SAT: $("#employeeScheduleSAT").val()
+                        },
+                        byEmployee : $("#employeeScheduleBy").val()
+                    }
+                },
+                dataType : 'json'
+            }).success(function(json) {
+                if (json.success == true) {
+                    timeOffCreateRequestHandler.loadCalendars( $("#employeeScheduleFor").val() );
+                } else {
+                    timeOffCommon.setEmployeeScheduleFormError( 'saveError' );
                 }
-            },
-            dataType : 'json'
-        }).success(function(json) {
-            if (json.success == true) {
-                timeOffCreateRequestHandler.loadCalendars( $("#employeeScheduleFor").val() );
-            } else {
-                alert( "There was an error saving the new schedule." );
-            }
-            return;
-        }).error(function() {
-            alert( "There was an error uploading the new schedule." );
-            return;
-        });
+                return;
+            }).error(function() {
+                timeOffCommon.setEmployeeScheduleFormError( 'uploadError' );
+                return;
+            });
+        } else {
+            timeOffCommon.setEmployeeScheduleFormError( 'error' );
+        }
     }
     
     /**
@@ -101,6 +178,11 @@ var timeOffCommon = new function ()
         }, 1000);
     }
     
+    /**
+     * Handles the site navigation menus on hover.
+     * 
+     * @returns {undefined}
+     */
     this.autoOpenDropdownOnHover = function() {
         $(".dropdown").hover(            
             function() {
