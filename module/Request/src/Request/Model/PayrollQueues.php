86ba5420076dcaa332425fb49017fc061d316de6
Kevin Sawicke <kevin_sawicke@swifttrans.com>
2016-05-18 15:26:40 -0700

    Adjust count for DataTables for By Status Queue when date range used.

--- module/Request/src/Request/Model/PayrollQueues.php
+++ module/Request/src/Request/Model/PayrollQueues.php
@@ -496,11 +496,37 @@ class PayrollQueues extends BaseDB {
     
     public function countByStatusQueueItems( $data = null, $isFiltered = false )
     {
-        $rawSql = "SELECT COUNT(*) AS RCOUNT       
-        FROM TIMEOFF_REQUESTS request
-        INNER JOIN PRPMS employee ON employee.PREN = request.EMPLOYEE_NUMBER
-        INNER JOIN PRPSP manager ON employee.PREN = manager.SPEN
-        INNER JOIN PRPMS manager_addons ON manager_addons.PREN = manager.SPSPEN";
+        $rawSql = "SELECT COUNT(*) AS RCOUNT FROM (
+            SELECT
+                ROW_NUMBER () OVER (ORDER BY MIN_DATE_REQUESTED ASC, EMPLOYEE_LAST_NAME ASC) AS ROW_NUMBER,
+                DATA.* FROM (
+                SELECT
+                request.REQUEST_ID AS REQUEST_ID,
+                TRIM(request.EMPLOYEE_NUMBER) AS EMPLOYEE_NUMBER,
+                request.REQUEST_REASON AS REQUEST_REASON,
+                status.DESCRIPTION AS REQUEST_STATUS_DESCRIPTION,
+                (
+                    SELECT SUM(requested_hours) FROM timeoff_request_entries entry WHERE entry.request_id = request.request_id
+                ) AS REQUESTED_HOURS,
+                (
+                    SELECT MIN(REQUEST_DATE) FROM timeoff_request_entries entry WHERE entry.request_id = request.request_id
+                ) AS MIN_DATE_REQUESTED,
+                (
+                    SELECT MAX(REQUEST_DATE) FROM timeoff_request_entries entry WHERE entry.request_id = request.request_id
+                ) AS MAX_DATE_REQUESTED,
+
+                TRIM(employee.PRLNM) CONCAT ', ' CONCAT TRIM(employee.PRFNM) CONCAT ' (' CONCAT TRIM(employee.PREN) CONCAT ')' as EMPLOYEE_DESCRIPTION,
+                TRIM(employee.PRFNM) AS EMPLOYEE_FIRST_NAME,
+                TRIM(employee.PRLNM) AS EMPLOYEE_LAST_NAME,
+		TRIM(manager_addons.PRLNM) CONCAT ', ' CONCAT TRIM(manager_addons.PRFNM) CONCAT ' (' CONCAT TRIM(manager_addons.PREN) CONCAT ')' as APPROVER_QUEUE,
+                TRIM(manager_addons.PREML1) AS MANAGER_EMAIL_ADDRESS
+            FROM TIMEOFF_REQUESTS request
+            INNER JOIN PRPMS employee ON employee.PREN = request.EMPLOYEE_NUMBER
+            INNER JOIN PRPSP manager ON employee.PREN = manager.SPEN
+            INNER JOIN PRPMS manager_addons ON manager_addons.PREN = manager.SPSPEN
+            INNER JOIN TIMEOFF_REQUEST_STATUSES status ON status.REQUEST_STATUS = request.REQUEST_STATUS
+            ORDER BY REQUEST_STATUS_DESCRIPTION ASC, MIN_DATE_REQUESTED ASC, EMPLOYEE_LAST_NAME ASC) AS DATA
+        ) AS DATA2";
         
         $where = [];
             
@@ -510,6 +536,12 @@ class PayrollQueues extends BaseDB {
                               employee.PRFNM LIKE '%" . strtoupper( $data['search']['value'] ) . "%' OR
                               employee.PRLNM LIKE '%" . strtoupper( $data['search']['value'] ) . "%' 
                             )";
+            }
+            if( array_key_exists( 'startDate', $data) && !empty( $data['startDate'] ) ) {
+                $where[] = "MIN_DATE_REQUESTED >= '" . $data['startDate'] . "'";
+            }
+            if( array_key_exists( 'endDate', $data) && !empty( $data['endDate'] ) ) {
+                $where[] = "MAX_DATE_REQUESTED <= '" . $data['endDate'] . "'";
             }
         }
         $rawSql .=  ( !empty( $where ) ? " WHERE " . implode( " AND ", $where ) : "" );
