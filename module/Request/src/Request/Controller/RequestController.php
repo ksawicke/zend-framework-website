9804966acf5a1c530f9d81268ae1baee52991f3c
Kevin Sawicke <kevin_sawicke@swifttrans.com>
2016-07-06 16:02:06 -0700

    Add form to allow managing email overrides

--- module/Request/src/Request/Controller/RequestController.php
+++ module/Request/src/Request/Controller/RequestController.php
@@ -178,6 +178,20 @@ class RequestController extends AbstractActionController
                                ]
         ]);
     }
+    
+    public function manageEmailOverridesAction()
+    {
+        $Employee = new \Request\Model\Employee();
+        
+        return new ViewModel([
+            'isPayrollAdmin' => \Login\Helper\UserSession::getUserSessionVariable('IS_PAYROLL'),
+            'flashMessages' => ['success' => $this->flashMessenger()->getCurrentSuccessMessages(),
+                                'warning' => $this->flashMessenger()->getCurrentWarningMessages(),
+                                'error' => $this->flashMessenger()->getCurrentErrorMessages(),
+                                'info' => $this->flashMessenger()->getCurrentInfoMessages()
+                               ]
+        ]);
+    }
 
     /**
      * Allows an employee to submit a new time off request for themselves.
