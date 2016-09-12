f6080721a55d994a6e27f3ffb939c86e1f2416c6
Guido Faecke <guido_faecke@swifttrans.com>
2016-09-12 15:18:23 -0700

    aaa

--- module/Login/src/Login/Mapper/LoginMapperInterface.php
+++ module/Login/src/Login/Mapper/LoginMapperInterface.php
@@ -6,14 +6,16 @@ use Login\Model\LoginInterface;
 interface LoginMapperInterface
 {
     public function authenticateUser($username = null, $password = null);
-    
+
     public function isManager($employeeNumber = null);
-    
+
     public function isPayroll($employeeNumber = null);
-    
+
     public function isPayrollAdmin($employeeNumber = null);
-    
+
     public function isPayrollAssistant($employeeNumber = null);
-    
+
     public function isProxy($employeeNumber = null);
+
+    public function isProxyForManager($employeeNumber = null);
 }
