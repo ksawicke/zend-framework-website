69193ba3aca977504e62e9f6fae8c316989b35e8
Guido Faecke <guido_faecke@swifttrans.com>
2016-09-19 13:30:25 -0700

    path missing in URL

--- public/convertStatusPendingToCompleted.php
+++ public/convertStatusPendingToCompleted.php
@@ -7,6 +7,6 @@ $currentPath = getcwd();
 
 $ch = curl_init();
 
-curl_setopt($ch, CURLOPT_URL, 'http://' . $currentSystem . ':10080' . substr($currentPath, 20) . '/public/api/set_pending_transaction_to_completed');
+curl_setopt($ch, CURLOPT_URL, 'http://' . $currentSystem . ':10080' . substr($currentPath, 20) . '/public/api/scheduler/set_pending_transaction_to_completed');
 $data = curl_exec($ch);
 curl_close($ch);
