af3d7447a4c70ad81817df09c94f2a6a712b3b73
Guido Faecke <guido_faecke@swifttrans.com>
2016-10-14 14:02:53 -0700

    done

--- module/Request/src/Request/Controller/RequestController.php
+++ module/Request/src/Request/Controller/RequestController.php
@@ -477,6 +477,9 @@ class RequestController extends AbstractActionController
         $request->getHeader('referer');
         $referredPage = $request->getQuery('q');
 
+        $fromQueue = $this->params()->fromRoute('fromQueue');
+//         var_dump($fromQueue);
+
         $requestId = $this->params()->fromRoute('request_id');
         $Employee = new Employee();
         $TimeOffRequests = new TimeOffRequests();
@@ -488,6 +491,7 @@ class RequestController extends AbstractActionController
 
         $isPayroll = \Login\Helper\UserSession::getUserSessionVariable( 'IS_PAYROLL' );
         $viewQueueLink = '';
+
         switch( $timeOffRequestData['REQUEST_STATUS_DESCRIPTION'] ) {
             case "Pending Manager Approval":
                 $viewQueueLink = '/request/view-manager-queue/pending-manager-approval';
@@ -507,6 +511,10 @@ class RequestController extends AbstractActionController
             case "Update Checks":
                 $viewQueueLink = '/request/view-payroll-queue/update-checks';
                 break;
+        }
+
+        if ($fromQueue !== null) {
+            $viewQueueLink = '/request/view-payroll-queue/by-status';
         }
 
         return new ViewModel( [
